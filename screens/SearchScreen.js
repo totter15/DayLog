d139b21f8f7c9a3c37087bc715b97ecdc3c31a94
천진아 <jina@cheonjin-aui-MacBookPro.local>
2022-04-04 13:32:34 +0900

    feat:SearchScreen 빈 검색어 화면

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -3,6 +3,7 @@ import {StyleSheet, View, Text} from 'react-native';
 import LogContext from '../contexts/LogContext';
 import SearchContext from '../contexts/SearchContext';
 import FeedList from '../components/FeedList';
+import EmptySearchResult from '../android/EmptySearchResult';
 
 const SearchScreen = ({navigation}) => {
   const {keyword} = useContext(SearchContext);
@@ -14,6 +15,13 @@ const SearchScreen = ({navigation}) => {
       : logs.filter(log =>
           [log.title, log.body].some(text => text.includes(keyword)),
         );
+
+  if (keyword === '') {
+    return <EmptySearchResult type="EMPTY_KEYWORD" />;
+  }
+  if (filtered.length === 0) {
+    return <EmptySearchResult type="NOT_FOUND" />;
+  }
 
   return (
     <View style={styles.block}>
